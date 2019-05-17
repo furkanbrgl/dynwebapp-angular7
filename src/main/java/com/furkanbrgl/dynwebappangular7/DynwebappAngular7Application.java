8c611f9eb68f78cd892a2d7ee5ecffeb85e5ab39
furkanbrgl <furkan.birgul@inomera.com>
2019-05-17 19:04:25 +0300

    Initial commit

---
+++ src/main/java/com/furkanbrgl/dynwebappangular7/DynwebappAngular7Application.java
@@ -0,0 +1,13 @@
+package com.furkanbrgl.dynwebappangular7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynwebappAngular7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynwebappAngular7Application.class, args);
+	}
+
+}
